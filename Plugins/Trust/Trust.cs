345c77ce757d9055584102fc461a5efb1f43add9
Leizuohong <lzh791397845@gmail.com>
2021-01-07 07:41:58 +0800

    123

---
+++ Plugins/Trust/Trust.cs
@@ -0,0 +1,170 @@
+﻿using Buddy.Coroutines;
+using ff14bot;
+using ff14bot.AClasses;
+using ff14bot.Behavior;
+using ff14bot.Helpers;
+using ff14bot.Managers;
+using ff14bot.NeoProfiles;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using TreeSharp;
+
+namespace Trust
+{
+    public class Trust : BotPlugin
+    {
+        private Composite _coroutine;
+        private TrustSettings _settingsForm;
+        private static uint _buff = 48;
+
+        public override string Author { get { return "athlon"; } }
+#if RB_CN
+        public override string Name => "亲信战友";
+#else
+        public override string Name => "Trust";
+#endif
+        public override Version Version { get { return new Version(1, 2, 0); } }
+
+        private bool CanTrust() => Array.IndexOf(new int[] { 837, 821, 823, 836, 822, 838, 884 }, WorldManager.ZoneId) >= 0;
+        public override void OnInitialize()
+        {
+            if (PluginManager.Plugins.Where(p => p.Plugin.Name == "SideStep" || p.Plugin.Name == "回避").Any())
+            {
+                var _plugin = PluginManager.Plugins.Where(p => p.Plugin.Name == "SideStep" || p.Plugin.Name == "回避").FirstOrDefault();
+                if (_plugin.Enabled == false) { _plugin.Enabled = true; }
+            }
+
+            _coroutine = new Decorator(c => CanTrust(), new ActionRunCoroutine(r => RunTrust()));
+        }
+
+        public override void OnEnabled()
+        {
+            TreeRoot.OnStart += OnBotStart;
+            TreeRoot.OnStop += OnBotStop;
+            TreeHooks.Instance.OnHooksCleared += OnHooksCleared;
+
+            if (TreeRoot.IsRunning) { AddHooks(); }
+        }
+
+        public override void OnDisabled()
+        {
+            TreeRoot.OnStart -= OnBotStart;
+            TreeRoot.OnStop -= OnBotStop;
+            RemoveHooks();
+        }
+
+        public override void OnShutdown() { OnDisabled(); }
+
+        public override bool WantButton { get { return true; } }
+
+        public override void OnButtonPress()
+        {
+            if (_settingsForm == null || _settingsForm.IsDisposed || _settingsForm.Disposing) { _settingsForm = new TrustSettings(); }
+            _settingsForm.ShowDialog();
+        }
+
+        private void AddHooks()
+        {
+            Logging.Write(Colors.Aquamarine, "Adding Trust Hook");
+            TreeHooks.Instance.AddHook("TreeStart", _coroutine);
+        }
+
+        private void RemoveHooks()
+        {
+            Logging.Write(Colors.Aquamarine, "Removing Trust Hook");
+            TreeHooks.Instance.RemoveHook("TreeStart", _coroutine);
+        }
+
+        private void OnBotStop(BotBase bot) { RemoveHooks(); }
+
+        private void OnBotStart(BotBase bot) { AddHooks(); }
+
+        private void OnHooksCleared(object sender, EventArgs e) { RemoveHooks(); }
+
+        private static async Task<bool> EatFood()
+        {
+            if (Settings.Instance.Id == 0 || !InventoryManager.FilledSlots.ContainsFooditem(Settings.Instance.Id)) { return false; }
+
+            if (GatheringManager.WindowOpen) { return false; }
+
+            var item = InventoryManager.FilledSlots.GetFoodItem(Settings.Instance.Id);
+
+            if (item == null) return false;
+
+            Logging.Write(Colors.Aquamarine, "[吃食物] Eating " + item.Name);
+            item.UseItem();
+            await Coroutine.Wait(5000, () => Core.Player.HasAura(_buff));
+
+            return true;
+        }
+        private static async Task<bool> PlayerCheck()
+        {
+            if (Core.Me.CurrentHealthPercent <= 0)
+            {
+#if RB_CN
+                Logging.Write(Colors.Aquamarine, $"检测到死亡");
+#else
+                Logging.Write(Colors.Aquamarine, $"Player has died.");
+#endif
+                await Coroutine.Sleep(10000);
+                NeoProfileManager.Load(NeoProfileManager.CurrentProfile.Path, true);
+                NeoProfileManager.UpdateCurrentProfileBehavior();
+                await Coroutine.Sleep(5000);
+                return true;
+            }
+
+            return false;
+        }
+        private async Task<bool> RunTrust()
+        {
+            if (!Core.Me.InCombat && ActionManager.IsSprintReady && MovementManager.IsMoving)
+            {
+                ActionManager.Sprint();
+                await Coroutine.Wait(1000, () => !ActionManager.IsSprintReady);
+            }
+
+            if (!Core.Player.HasAura(_buff)) { await EatFood(); }
+
+            switch (WorldManager.ZoneId)
+            {
+                case 837: //71本 水滩村
+                    return await HolminsterSwitch.Run();
+                case 821: //73本 多恩美格禁园
+                    if (await PlayerCheck())  {  return true; }
+                    return await DohnMheg.Run();
+                case 823: //75本 奇坦那神影洞
+                    if (await PlayerCheck())  {  return true; }
+                    return await TheQitanaRavel.Run();
+                case 836: //77本 马利卡大井
+                    if (await PlayerCheck())  {  return true; }
+                    return await MalikahWell.Run();
+                case 822: //79本 格鲁格火山
+                    if (await PlayerCheck())  {  return true; }
+                    return await MtGulg.Run();
+                case 838: //80本 亚马乌罗提
+                    if (await PlayerCheck())  {  return true; }
+                    return await Amaurot.Run();
+                case 884: //80本 国际服 5.1
+                    if (await PlayerCheck())  {  return true; }
+                    return await TheGrandCosmos.Run();
+                default:
+                    return false;
+            }
+        }
+    }
+
+    public class Settings : JsonSettings
+    {
+        private static Settings _instance;
+        public static Settings Instance { get { return _instance ?? (_instance = new Settings()); ; } }
+
+        public Settings() : base(Path.Combine(CharacterSettingsDirectory, "Trust.json")) { }
+
+        [Setting]
+        public uint Id { get; set; }
+    }
+}
--- default.py
+++ default.py
@@ -1,8 +1,11 @@
 # -*- coding: UTF-8 -*-
 # Public package
 import os
+import re
 # Private package
 import headpy.hfile as hfile
+
+# base plugins
 
 
 def get_exbuddy():
@@ -21,6 +24,8 @@ def get_exbuddy():
                     name='ExBuddy')
     os.chdir('..')
 
+# addition botbase
+
 
 def get_llamalibrary():
     os.chdir('temp')
@@ -34,6 +39,23 @@ def get_llamalibrary():
     hfile.rm_folder(path='.',
                     name='LlamaLibrary')
     os.chdir('..')
+
+# addition plugin for retainer in lisbeth
+
+
+def get_llamalibrary_retainer():
+    os.chdir('temp')
+    os.system('git clone https://github.com/nt153133/LisbethVentures.git')
+    os.mkdir('../Plugins/LisbethVentures')
+    hfile.copy_file(source_path='LisbethVentures',
+                    source_name='LisbethVentures.cs',
+                    path='../Plugins/LisbethVentures',
+                    name='LisbethVentures.cs')
+    hfile.rm_folder(path='.',
+                    name='LisbethVentures')
+    os.chdir('..')
+
+# addition profile for relicweapon
 
 
 def get_relicweapon():
@@ -51,4 +73,64 @@ def get_relicweapon():
                     name='ExTurnInGuildLeveTag.cs')
     hfile.rm_folder(path='.',
                     name='ZodiacWeapons')
+    os.chdir('..')
+
+# addition profile
+
+
+def get_sodimm():
+    os.chdir('temp')
+    os.system('git clone https://github.com/sodimm/RebornBuddy.git')
+    hfile.copy_folder(source_path='RebornBuddy/Profiles',
+                      source_name='Sodimm',
+                      path='../Profiles',
+                      name='Sodimm')
+    hfile.copy_folder(source_path='RebornBuddy/Downloads/Latest',
+                      source_name='Sparrow',
+                      path='../Plugins',
+                      name='Sparrow')
+    hfile.rm_folder(path='.',
+                    name='RebornBuddy')
+    os.chdir('..')
+
+# addition profile
+
+
+def get_y2krazy():
+    os.chdir('temp')
+    os.system('git clone https://github.com/y2krazy/Rebornbuddy-Profiles.git')
+    hfile.rm_folder(path='Rebornbuddy-Profiles',
+                    name='.git')
+    hfile.copy_folder(source_path='.',
+                      source_name='Rebornbuddy-Profiles',
+                      path='../Profiles',
+                      name='y2krazy')
+    hfile.rm_folder(path='.',
+                    name='Rebornbuddy-Profiles')
+    os.chdir('..')
+
+# addition profile for lv.70-80
+
+
+def get_rbtrust():
+    os.chdir('temp')
+    os.system('git clone https://github.com/athlon18/RBtrust.git')
+    for file in os.listdir('RBtrust/Quest Behaviors'):
+        hfile.copy_file(source_path='RBtrust/Quest Behaviors',
+                        source_name=file,
+                        path='../Quest Behaviors',
+                        name=file)
+    hfile.copy_folder(source_path='RBtrust/Plugins',
+                      source_name='Trust',
+                      path='../Plugins',
+                      name='Trust')
+    os.mkdir('../Profiles/Trust')
+    for file in os.listdir('RBtrust'):
+        if(re.match(r'(.*)全自动(.*)', file)):
+            hfile.copy_file(source_path='RBtrust',
+                            source_name=file,
+                            path='../Profiles/Trust',
+                            name=file)
+    hfile.rm_folder(path='.',
+                    name='RBtrust')
     os.chdir('..')
